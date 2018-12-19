ad0b297324a1bf74f4e0a3dcdc15cc2bcb8367e1
Ciprian Focsaneanu <ciprian.focsaneanu@keysight.com>
2018-12-19 14:34:47 +0200

    Add dummy navbar

--- medical-prescriptions/src/app/app.module.ts
+++ medical-prescriptions/src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthGuardService } from './core/auth-guard.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { CustomMaterialModule } from './material.module';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { AuthGuardService } from './core/auth-guard.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CustomMaterialModule
   ],
   providers: [
     AuthGuardService
